86a16991778a4d17882f51fde40cc10898d872d8
wangxun <wangxun@cyai.com>
2022-08-31 15:53:03 +0800

    feat:init

---
+++ src/angular-schematics-tutorial/index_spec.ts
@@ -0,0 +1,16 @@
+import { Tree } from '@angular-devkit/schematics';
+import { SchematicTestRunner } from '@angular-devkit/schematics/testing';
+import * as path from 'path';
+
+const collectionPath = path.join(__dirname, '../collection.json');
+
+describe('angular-schematics-tutorial', () => {
+  it('works', async () => {
+    const runner = new SchematicTestRunner('schematics', collectionPath);
+    const tree = await runner
+      .runSchematicAsync('angular-schematics-tutorial', {}, Tree.empty())
+      .toPromise();
+
+    expect(tree.files).toEqual([]);
+  });
+});
